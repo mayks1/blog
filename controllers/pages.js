93a48e07e93629360a9f1b56673a8bc0fb369954
mayks <mayks1@gmail.com>
2019-08-14 18:34:45 +0300

    Add posibility to write data to a file

--- controllers/pages.js
+++ controllers/pages.js
@@ -1,20 +1,31 @@
-const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
-const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
-const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
+const fs        = require('fs');
+const path      = require('path');
 
+const homeStartingContent = "Тук ще се опитам да ви покажа как можете да работите по-ефективно и да анализирате по-добре своите данни, чрез възможностите, които предоставя Express.JS ."
+const contactContent = "За да се свържете с мен, моля използвайте формата за контакти:"
+const aboutContent = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
 const _ = require('lodash');
+const dataPath = path.join(path.dirname(process.mainModule.filename), 'data', 'posts.json');
 
-// Array of the Posts
 let posts = [];
 
+fs.readFile(dataPath, (err, fileContent) => {
+  if (err) {
+    posts = [];
+  }
+  posts = JSON.parse(fileContent);
+});
+
+
+
 exports.getHome = (req, res) => {
-  
+
     res.render('home', { 
       pageTitle : 'Начало',
       startingContent: homeStartingContent,
       posts: posts
      });
-}
+};
 
 exports.getAbout = (req, res) => {
     res.render('about', { 
@@ -44,7 +55,9 @@ exports.postCompose = (req, res) => {
 
   // Add post to the Array
   posts.push(post);
-
+  fs.writeFile(dataPath, JSON.stringify(posts), (err) => {
+    console.log(err);
+  });
   res.redirect('/');
 }
 
