0562558a9da995ac35b377e5acddfd44562f7dfa
mayks <mayks1@gmail.com>
2019-08-27 18:31:49 +0300

    Added Final Responsive Menu

---
+++ public/js/nav.js
@@ -0,0 +1,9 @@
+const hamburger = document.querySelector('.menu-toggle');
+
+const nav = document.querySelector('nav');
+
+hamburger.addEventListener('click', () => {
+
+    nav.classList.toggle('active');
+    
+});
